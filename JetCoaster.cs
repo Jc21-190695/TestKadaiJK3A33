75ac12e74e2b8a3b20a1b427cc878cab65b9c8b7
謙太 船山 <190695@jc-21.jp>
2022-02-18 15:37:31 +0900

    プロジェクト ファイルを追加します。

---
+++ JetCoaster.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jetcoster
+{
+    public class JetCoaster
+    {
+        public static int Ride
+            (int k, int[] g, int r)
+        {
+            int[] vs = new int[g.Length];
+            Array.Copy(g, vs, g.Length);
+
+            int add = 0;
+            int space = 0;
+            int limit = k;
+
+            for (int w = 0; w < r; w++)
+            {
+                k = limit;
+                int count = 0;
+
+                for (int i = 0; i < g.Length; i++)
+                {
+                    if (k >= g[i])
+                    {
+                        k = k - g[i];
+                        add = add + g[i];
+                        count = count + 1;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                for (int j = 0; j < count; j++)
+                {
+                    space = g[0];
+                    for (int q = 0; q < g.Length; q++)
+                    {
+                        if (q == g.Length - 1)
+                        {
+                            g[q] = space;
+                        }
+                        else
+                        {
+                            g[q] = g[q + 1];
+                        }
+                    }
+                }
+            }
+            Array.Copy(vs, g, vs.Length);
+            return add;
+        }
+    }
+}
---
+++ kuriage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jetcoster
+{
+    public class Kuriage
+    {
+        public static int Kuriage10(int j)
+        {
+            double i, k, p;
+            int q;
+            if (j >= 0)
+            {
+
+                i = (double)j;
+
+                k = i / 10;
+
+                p = Math.Ceiling(k);
+
+                q = (int)p * 10;
+            }
+            else
+            {
+                i = (double)j;
+
+                k = i / 10;
+
+                p = Math.Floor(k);
+
+                q = (int)p * 10;
+            }
+
+            return q;
+        }
+    }
+}
